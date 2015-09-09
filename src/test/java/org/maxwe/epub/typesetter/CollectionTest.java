4727af6edd21bfc747bda6f13c6041b8ceef6f5d
DingPengwei <www.dingpengwei@gmail.com>
2015-09-09 20:48:47 +0800

    1

---
+++ src/test/java/org/maxwe/epub/typesetter/CollectionTest.java
@@ -0,0 +1,9 @@
+package org.maxwe.epub.typesetter;
+
+/**
+ * Created by Pengwei Ding on 2015-09-09 19:17.
+ * Email: www.dingpengwei@foxmail.com www.dingpegnwei@gmail.com
+ * Description: @TODO
+ */
+public class CollectionTest {
+}
