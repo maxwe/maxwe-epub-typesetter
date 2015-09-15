6e55f5ec3a80cb619bb998153f90ba9462e9fcf3
DingPengwei <www.dingpengwei@gmail.com>
2015-09-15 19:47:54 +0800

    6

--- src/main/java/org/maxwe/epub/typesetter/impl/ParagraphTypesetter.java
+++ src/main/java/org/maxwe/epub/typesetter/impl/ParagraphTypesetter.java
@@ -1,7 +1,16 @@
 package org.maxwe.epub.typesetter.impl;
 
 import org.maxwe.epub.parser.core.IChapter;
+import org.maxwe.epub.parser.core.IParagraph;
+import org.maxwe.epub.parser.core.ISection;
+import org.maxwe.epub.parser.impl.Audio;
+import org.maxwe.epub.parser.impl.Image;
+import org.maxwe.epub.parser.impl.Text;
+import org.maxwe.epub.parser.impl.Video;
 import org.maxwe.epub.typesetter.core.IChapterTypesetter;
+import org.maxwe.epub.typesetter.core.ISectionTypesetter;
+
+import java.util.LinkedList;
 
 /**
  * Created by Pengwei Ding on 2015-09-15 16:40.
@@ -10,10 +19,47 @@ import org.maxwe.epub.typesetter.core.IChapterTypesetter;
  */
 public class ParagraphTypesetter extends ASectionTypesetter  {
 
+    private LinkedList<ISectionTypesetter> sectionTypesetters = new LinkedList<ISectionTypesetter>();
+
     @Override
     public void typeset(IChapterTypesetter chapterTypesetter) {
-        int paragraphOffset = chapterTypesetter.getParagraphOffset();
         IChapter chapter = chapterTypesetter.getChapter();
         int paragraphLength = chapter.getParagraphLength();
+        int paragraphOffset = chapterTypesetter.getParagraphOffset();
+        int offset = chapterTypesetter.getOffset();
+
+        /**
+         * 若段落偏移量在段落长度范围内表示该章节还有段落没有排版完成
+         */
+        while (this.currentY <= this.endY && paragraphOffset < paragraphLength){
+            IParagraph paragraph = chapter.getParagraph(paragraphOffset);
+            int sectionLength = paragraph.getSectionLength();
+            /**
+             * 若片段偏移量在片段长度范围内表示该段落还有片段没有排版完成
+             * 终止条件1：超出排版区域范围；2：超出排版内容范围
+             */
+            while (this.currentY <= this.endY && chapterTypesetter.getSectionOffset() < sectionLength){
+                ISection section = paragraph.getSection(chapterTypesetter.getSectionOffset());
+                ISectionTypesetter sectionTypesetter = null;
+                if (section instanceof Text){
+                    sectionTypesetter = new TextTypesetter();
+                }else if (section instanceof Image){
+                    sectionTypesetter = new ImageTypesetter();
+                }else if (section instanceof Audio){
+                    sectionTypesetter = new AudioTypesetter();
+                }else if (section instanceof Video){
+                    sectionTypesetter = new VideoTypesetter();
+                }
+                sectionTypesetter.typeset(chapterTypesetter);
+                this.currentX = sectionTypesetter.getEndPoint()[0];
+                this.currentY = sectionTypesetter.getEndPoint()[1];
+                this.sectionTypesetters.add(sectionTypesetter);
+
+            }
+        }
+    }
+
+    public LinkedList<ISectionTypesetter> getSectionTypesetters() {
+        return sectionTypesetters;
     }
 }
--- src/main/java/org/maxwe/epub/typesetter/impl/TextTypesetter.java
+++ src/main/java/org/maxwe/epub/typesetter/impl/TextTypesetter.java
@@ -28,27 +28,22 @@ public class TextTypesetter extends ASectionTypesetter {
 
         /**
          * 行排版
+         * 终止条件1：超出排版区域范围；2：超出排版内容范围
          */
-        while (this.currentY + Configer.CONFIGER_WORD_SIZE <= this.endY) {
+        while (this.currentY + Configer.CONFIGER_WORD_SIZE <= this.endY && offset < originLength) {
             LinkedHashMap<Integer, String> lineWords = new LinkedHashMap<Integer, String>();
             /**
              * 列排版
              * 即行间排版
+             * 终止条件1：超出排版区域范围；2：超出排版内容范围
              */
-            while (this.currentX + Configer.CONFIGER_WORD_SIZE <= this.endX) {
+            while (this.currentX + Configer.CONFIGER_WORD_SIZE <= this.endX && offset < originLength) {
                 String word = text.getWord(offset);
                 lineWords.put(this.currentX, word);
                 /**
                  * 修改偏移量
                  */
                 chapterTypesetter.setOffset(offset++);
-                /**
-                 * 如果偏移量到结尾偏移量重置 片段偏移量增加一
-                 */
-                if (offset >= originLength) {
-                    offset = 0;
-                    chapterTypesetter.setSectionOffset(sectionOffset++);
-                }
                 /**
                  * X轴坐标的移动：当前字的位置+单个文字所占用大小+字间距=下一个字的位置
                  */
@@ -62,5 +57,7 @@ public class TextTypesetter extends ASectionTypesetter {
         }
     }
 
-
+    public LinkedHashMap<Integer, LinkedHashMap<Integer, String>> getWords() {
+        return words;
+    }
 }
