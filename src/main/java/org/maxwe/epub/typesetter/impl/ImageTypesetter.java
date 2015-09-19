cd5a32280116e15de8d59b54444878429d675226
DingPengwei <www.dingpengwei@gmail.com>
2015-09-19 21:11:45 +0800

    typesetter plain image

--- src/main/java/org/maxwe/epub/typesetter/impl/ImageTypesetter.java
+++ src/main/java/org/maxwe/epub/typesetter/impl/ImageTypesetter.java
@@ -1,16 +1,68 @@
 package org.maxwe.epub.typesetter.impl;
 
+import org.maxwe.epub.parser.core.ISection;
+import org.maxwe.epub.parser.impl.Image;
 import org.maxwe.epub.typesetter.core.IChapterTypesetter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Created by Pengwei Ding on 2015-09-13 09:15.
  * Email: www.dingpengwei@foxmail.com www.dingpegnwei@gmail.com
- * Description: @TODO
+ * Description: 图片排版 计算图片的方式受限于android平台的相关API
  */
 public class ImageTypesetter extends ASectionTypesetter {
 
+    private String imagePath;
+
+    public String getImagePath() {
+        return this.imagePath;
+    }
+
     @Override
     public void typeset(IChapterTypesetter chapterTypesetter) {
+        int paragraphOffset = chapterTypesetter.getParagraphOffset();
+        int sectionOffset = chapterTypesetter.getSectionOffset();
+        int offset = chapterTypesetter.getOffset();
 
+        ISection section = chapterTypesetter.getChapter().getParagraph(paragraphOffset).getSection(sectionOffset);
+
+        Image image = (Image) section;
+        this.imagePath = image.getMediaPath();
+
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(new File(this.imagePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+
+        this.currentX = this.currentX + width;
+        this.currentY = this.currentY + height;
+
+        chapterTypesetter.setOffset(0);
+        chapterTypesetter.setSectionOffset(chapterTypesetter.getSectionOffset() + 1);
+
+    }
+
+    /**
+     * 使用占位符矩阵的方式打印图像
+     */
+    public void print() {
+        for (int i = 0; i < 20; i++) {
+            if (i==10){
+                System.out.print(this.imagePath);
+            }else {
+                for (int j=0;j<50;j++){
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/test/java/org/maxwe/epub/typesetter/SingleChapterTypesetter.java
+++ src/test/java/org/maxwe/epub/typesetter/SingleChapterTypesetter.java
@@ -8,6 +8,7 @@ import org.maxwe.epub.typesetter.core.APageTypesetter;
 import org.maxwe.epub.typesetter.core.AParagraphTypesetter;
 import org.maxwe.epub.typesetter.core.ISectionTypesetter;
 import org.maxwe.epub.typesetter.impl.ChapterTypesetter;
+import org.maxwe.epub.typesetter.impl.ImageTypesetter;
 import org.maxwe.epub.typesetter.impl.TextTypesetter;
 
 import java.io.File;
@@ -26,6 +27,7 @@ public class SingleChapterTypesetter extends TestCase {
 
     private String path = SingleChapterTypesetter.class.getResource("/").getPath() + "sample";
     /*
+    纯文本排版测试
     取值第三章
     章节文件名称ds00216101.xhtml
     章节共有纯文本段落14个
@@ -69,6 +71,47 @@ public class SingleChapterTypesetter extends TestCase {
                                 }
                                 System.out.println("\n");
                             }
+                        }
+                    }
+                }
+            }
+        } else {
+            assertFalse("测试文件不存在", true);
+        }
+    }
+
+    /*
+    纯图片排版测试
+    取值第一章
+    章节文件名称Cover.xhtml
+    章节共有纯图片段落1个
+    图片信息240x304
+     */
+    private final int INDEX_OF_PLAIN_IMAGE = 0;
+
+    @Test
+    public void testPlainImageTypesetter() throws Exception{
+        if (new File(this.path).exists()) {
+            Book book = new Book(this.path);
+            System.out.println("############ 章节名：" + book.getNavigations().get(INDEX_OF_PLAIN_IMAGE).getTitle() + "############");
+            System.out.println("############ 章节位置：" + book.getNavigations().get(INDEX_OF_PLAIN_IMAGE).getHref() + "############");
+            long start = System.nanoTime();
+            TypesetterManager.getInstance().typeset(new ChapterTypesetter(new Chapter(book.getNavigation(INDEX_OF_PLAIN_IMAGE).getHref())));
+            long duration = System.nanoTime() - start;
+            DecimalFormat decimalFormat = new DecimalFormat("$,###");
+            System.out.println("排版耗时：" + decimalFormat.format(duration).replace("$", "") + "毫微妙");
+            LinkedList<APageTypesetter> pageTypesetters = TypesetterManager.getInstance().getPages();
+            System.out.println("共有页数：" + pageTypesetters.size());
+
+            for (APageTypesetter pageTypesetter : pageTypesetters) {
+                System.out.println("===============分页线=================");
+                LinkedList<AParagraphTypesetter> paragraphTypesetters = pageTypesetter.getParagraphTypesetters();
+                for (AParagraphTypesetter paragraphTypesetter:paragraphTypesetters){
+                    System.out.println("------------分段线--------------");
+                    LinkedList<ISectionTypesetter> sectionTypesetters = paragraphTypesetter.getSectionTypesetters();
+                    for (ISectionTypesetter sectionTypesetter:sectionTypesetters){
+                        if (sectionTypesetter instanceof ImageTypesetter) {
+                            ((ImageTypesetter) sectionTypesetter).print();
                         }
                     }
                 }
