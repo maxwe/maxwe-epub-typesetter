29694c03afdb9068b88a4cf5ddb1229d951b8330
DingPengwei <www.dingpengwei@gmail.com>
2016-03-02 14:24:45 +0800

    fixed chapter index

---
+++ src/main/java/org/maxwe/epub/typesetter/Configure.java
@@ -0,0 +1,9 @@
+package org.maxwe.epub.typesetter;
+
+/**
+ * Created by Pengwei Ding on 2016-03-02 10:01.
+ * Email: www.dingpengwei@foxmail.com www.dingpegnwei@gmail.com
+ * Description: @TODO
+ */
+public class Configure {
+}
